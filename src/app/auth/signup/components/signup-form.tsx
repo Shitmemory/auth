6141955071d6d1e8bf8bc97893aa69d99b0656d1
Will <wgledhill197@gmail.com>
2025-04-29 20:16:16 +0100

    first commit

--- src/app/auth/signup/components/signup-form.tsx
+++ src/app/auth/signup/components/signup-form.tsx
@@ -1,5 +1,20 @@
+import { SignupInput, SignupSchema } from "@/validators/signup-validator";
+import { useForm } from "react-hook-form";
+import {valibotResolver} from "@hookform/resolvers/valibot";
+
 export default function SignupForm() {
+    const form = useForm<SignupInput>({
+        resolver: valibotResolver(SignupSchema),
+        defaultValues: { name: "", email: "", password: "", confirmPassword: "" }
+    })
+
+    const {} = form
+
+    const submit = async (values: SignupInput) => {
+        console.log(values);
+    }
+    
     return (
-        <div>SignupForm</div>
-    )
+         <div>SignupForm</div>
+         )
 }
